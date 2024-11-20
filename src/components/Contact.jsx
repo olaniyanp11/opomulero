964ce490e99427f91777b5e88b88769f82960294
olaniyanp11 <olaniyanp11@.com>
2024-11-20 10:22:48 +0100

    created the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,29 @@ export const Contact = () => {
     return (
         <div>
             <h2 className='font-bold text-[30px] text-secondary text-center'> Contact Us </h2>
-            <div className='flex flex-col'>
-                <div className="w-full ">
-                    <form action="" className='px-[30px] '>
+            <div className='flex flex-col md:flex-row md:items-center'>
+                <div className="w-full md:w-1/2">
+                    <form action="" className='px-[30px] mb-5  '>
                         <div>
-                            <label htmlFor="email" className='font-bold text-neutral-600'>Email</label>
-                            <input type="email" className='w-full border border-neutral-300 ' />
-                       </div>
-
+                            <label htmlFor="email" className='font-bold text-neutral-500 text-[14px]'>Email</label>
+                            <input type="email" className='w-full border border-neutral-300 rounded-lg p-2 outline-secondary' />
+                        </div>
+                        <div>
+                            <label htmlFor="email" className='font-bold text-neutral-500 text-[14px]'>Subject</label>
+                            <input type="text" className='w-full border border-neutral-300 rounded-lg p-2 outline-secondary' />
+                        </div>
+                        <div>
+                            <label htmlFor="email" className='font-bold text-neutral-500 text-[14px]'>Message</label>
+                            <textarea type="text" className='w-full border border-neutral-300 rounded-lg p-4 outline-secondary' />
+                        </div>
+                        <div>
+                            <div className="btn">Submit</div>
+                        </div>
                     </form>
                 </div>
-                <div> <img src={Call} alt="" className='w-full' /></div>
+                <div className='w-full md:w-1/2'>
+                    <img src={Call} alt="" className='w-full' />
+                </div>
             </div>
         </div>
     )
